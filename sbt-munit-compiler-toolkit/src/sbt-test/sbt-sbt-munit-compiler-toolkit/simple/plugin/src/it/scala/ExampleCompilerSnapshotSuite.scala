d85dfc6450f3109fcdb5ae96e1595f78f57790ff
Jack C. Viers <jackcviers@gmail.com>
2023-05-31 20:56:25 -0500

    Adds example snapshot test

--- sbt-munit-compiler-toolkit/src/sbt-test/sbt-sbt-munit-compiler-toolkit/simple/plugin/src/it/scala/ExampleCompilerSnapshotSuite.scala
+++ sbt-munit-compiler-toolkit/src/sbt-test/sbt-sbt-munit-compiler-toolkit/simple/plugin/src/it/scala/ExampleCompilerSnapshotSuite.scala
@@ -14,16 +14,7 @@ class ExampleCompilerSnapshotSuite extends SnapshotSuite, CompilerSuite:
                     |  val x = example("test")
                     |}
                     |""".stripMargin
-    val (tree, compiledSource, context) = compileToStringTreeAndTreeSourcessAndStringContext(source, Option("pickleQuotes"))
-    val pattern = """dotty\.tools\.dotc\.core\.Scopes\$MutableScope@.{0,8}""".r
-    val emptyScopePattern = """dotty\.tools\.dotc\.core\.Scopes\$EmptyScope\$@.{0,8}""".r
-    val strippedMutableScopes = pattern.replaceAllIn(context, "dotty.tools.dotc.core.Scopes.MutableScope@")
-    val strippedEmptyScopes = emptyScopePattern.replaceAllIn(strippedMutableScopes, "dotty.tools.dotc.core.ScopesEmptyScope@")
-    (
-      pattern.replaceAllIn(tree, "dotty.tools.dotc.core.Scopes.MutableScope@"),
-      source,
-      strippedEmptyScopes
-    )
+    compileToStringTreeAndStringContext(source, Option("pickleQuotes"))
   }
 
 end ExampleCompilerSnapshotSuite
