161f734b8beb08d28ffcba059e427598f0470479
Jack C. Viers <jackcviers@gmail.com>
2023-05-31 19:43:28 -0500

    Fixes the scripted tests

--- sbt-munit-compiler-toolkit/src/sbt-test/sbt-sbt-munit-compiler-toolkit/simple/plugin/src/it/scala/ExampleCompilerSnapshotSuite.scala
+++ sbt-munit-compiler-toolkit/src/sbt-test/sbt-sbt-munit-compiler-toolkit/simple/plugin/src/it/scala/ExampleCompilerSnapshotSuite.scala
@@ -14,7 +14,16 @@ class ExampleCompilerSnapshotSuite extends SnapshotSuite, CompilerSuite:
                     |  val x = example("test")
                     |}
                     |""".stripMargin
-    compileToStringTreeAndTreeSourcessAndStringContext(source, Option("pickleQuotes"))
+    val (tree, compiledSource, context) = compileToStringTreeAndTreeSourcessAndStringContext(source, Option("pickleQuotes"))
+    val pattern = """dotty\.tools\.dotc\.core\.Scopes\$MutableScope@.{0,8}""".r
+    val emptyScopePattern = """dotty\.tools\.dotc\.core\.Scopes\$EmptyScope\$@.{0,8}""".r
+    val strippedMutableScopes = pattern.replaceAllIn(context, "dotty.tools.dotc.core.Scopes.MutableScope@")
+    val strippedEmptyScopes = emptyScopePattern.replaceAllIn(strippedMutableScopes, "dotty.tools.dotc.core.ScopesEmptyScope@")
+    (
+      pattern.replaceAllIn(tree, "dotty.tools.dotc.core.Scopes.MutableScope@"),
+      source,
+      strippedEmptyScopes
+    )
   }
 
 end ExampleCompilerSnapshotSuite
